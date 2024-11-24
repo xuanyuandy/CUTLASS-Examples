64dcbd09dd20fb4a97a227e7b4a25291106ee853
leimao <dukeleimao@gmail.com>
2024-11-24 11:00:21 -0800

    Add Vector Copy

---
+++ examples/cute_vector_copy/tests/profile_vector_copy.cu
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+#include "test_utils.hpp"
+
+#include "cute_vector_copy.hpp"
+
+static auto const LAUNCH_VECTOR_COPY_FLOAT{launch_vector_copy<float>};
+
+static auto const POWER_OF_TWO_VALUES{::testing::Values(1 << 28)};
+
+TEST_P(TestVectorCopyFloat, TestVectorCopyFloat)
+{
+    MeasurePerformance(LAUNCH_VECTOR_COPY_FLOAT);
+}
+
+INSTANTIATE_TEST_SUITE_P(TestVectorCopyPowerOfTwo, TestVectorCopyFloat,
+                         POWER_OF_TWO_VALUES);
