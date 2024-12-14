2ad60d58b3923138a57bde48e2a809e9c170e2dc
leimao <dukeleimao@gmail.com>
2024-12-13 23:13:20 -0800

    Add Vectorized Memory Access

--- examples/cute_general_matrix_multiplication/cute_general_matrix_multiplication_naive_vectorized.cu
+++ examples/cute_general_matrix_multiplication/cute_general_matrix_multiplication_naive_vectorized.cu
@@ -8,6 +8,11 @@
 
 #include "cute_general_matrix_multiplication.hpp"
 
+constexpr int constexpr_log2(int n)
+{
+    return ((n < 2) ? 0 : 1 + constexpr_log2(n / 2));
+}
+
 // Modified from the official CuTe example:
 // https://github.com/NVIDIA/cutlass/blob/e1cd8c7866dd6de02b66a89879795e7d7301aacc/examples/cute/tutorial/sgemm_1.cu#L52
 // This implementation uses shared memory.
@@ -861,6 +866,137 @@ static cudaError_t gemm_base(int m, int n, int k, Alpha alpha, TA const* A,
     return cudaGetLastError();
 }
 
+template <class TA, class TB, class TC, class Alpha, class Beta, class AStride,
+          class BStride, class CStride, class VectorTypeA, class VectorTypeB>
+static cudaError_t
+gemm_base_new(int m, int n, int k, Alpha alpha, TA const* A, int ldA,
+              TB const* B, int ldB, Beta beta, TC* C, int ldC, AStride stride_A,
+              BStride stride_B, CStride stride_C, bool use_swizzle_A,
+              bool use_swizzle_B, cudaStream_t stream)
+{
+    // Define GEMM shape.
+    auto const M{m};
+    auto const N{n};
+    auto const K{k};
+    auto const gemm_shape{cute::make_shape(M, N, K)}; // (M, N, K)
+
+    // Define CTA size.
+    auto const bM{cute::Int<128>{}};
+    auto const bN{cute::Int<128>{}};
+    auto const bK{cute::Int<8>{}};
+    auto const cta_tiler{cute::make_shape(bM, bN, bK)}; // (BLK_M, BLK_N, BLK_K)
+
+    // Define smem layouts.
+    // smem_layout_A is (BLK_M, BLK_K) column-major.
+    // smem_layout_B is (BLK_N, BLK_K) column-major.
+    // smem_layout_C is (BLK_M, BLK_N) column-major.
+    auto const smem_shape_A{cute::make_shape(bM, bK)}; // (BLK_M, BLK_K)
+    auto const smem_stride_A{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(smem_shape_A))}; // column-major
+    auto const smem_layout_A{
+        cute::make_layout(smem_shape_A, smem_stride_A)}; // (BLK_M, BLK_K)
+    auto const smem_shape_B{cute::make_shape(bN, bK)};   // (BLK_N, BLK_K)
+    auto const smem_stride_B{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(smem_shape_B))}; // column-major
+    auto const smem_layout_B{
+        cute::make_layout(smem_shape_B, smem_stride_B)}; // (BLK_N, BLK_K)
+    auto const smem_shape_C{cute::make_shape(bM, bN)};   // (BLK_M, BLK_N)
+    auto const smem_stride_C{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(smem_shape_C))}; // column-major
+    auto const smem_layout_C{
+        cute::make_layout(smem_shape_C, smem_stride_C)}; // (BLK_M, BLK_N)
+
+    // Define thread layouts.
+    auto const thread_shape_A{
+        cute::make_shape(cute::Int<32>{}, cute::Int<8>{})}; // (THR_M, THR_K)
+    auto const thread_shape_B{
+        cute::make_shape(cute::Int<32>{}, cute::Int<8>{})}; // (THR_N, THR_K)
+    auto const thread_shape_C{
+        cute::make_shape(cute::Int<16>{}, cute::Int<16>{})}; // (THR_M, THR_N)
+    auto const thread_stride_A{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(thread_shape_A))}; // column-major
+    auto const thread_stride_B{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(thread_shape_B))}; // column-major
+    auto const thread_stride_C{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(thread_shape_C))}; // column-major
+    auto const thread_layout_A{
+        cute::make_layout(thread_shape_A, thread_stride_A)}; // (THR_M, THR_K)
+    auto const thread_layout_B{
+        cute::make_layout(thread_shape_B, thread_stride_B)}; // (THR_N, THR_K)
+    auto const thread_layout_C{
+        cute::make_layout(thread_shape_C, thread_stride_C)}; // (THR_M, THR_N)
+    CUTE_STATIC_ASSERT_V(cute::size(thread_layout_A) ==
+                         cute::size(thread_layout_B));
+    CUTE_STATIC_ASSERT_V(cute::size(thread_layout_A) ==
+                         cute::size(thread_layout_C));
+
+    constexpr auto NUM_VECTOR_ELEMENTS_A{sizeof(VectorTypeA) / sizeof(TA)};
+    auto const vector_shape_A{
+        cute::make_shape(cute::Int<NUM_VECTOR_ELEMENTS_A>{},
+                         cute::Int<1>{})}; // (NUM_VECTOR_ELEMENTS_A, 1)
+    auto const vector_stride_A{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(vector_shape_A))}; // column-major
+    auto const vector_layout_A{cute::make_layout(
+        vector_shape_A, vector_stride_A)}; // (NUM_VECTOR_ELEMENTS_A, 1)
+    auto const NUM_VECTOR_ELEMENTS_B{sizeof(VectorTypeB) / sizeof(TB)};
+    auto const vector_shape_B{
+        cute::make_shape(cute::Int<NUM_VECTOR_ELEMENTS_B>{},
+                         cute::Int<1>{})}; // (NUM_VECTOR_ELEMENTS_B, 1)
+    auto const vector_stride_B{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(vector_shape_B))}; // column-major
+    auto const vector_layout_B{cute::make_layout(
+        vector_shape_B, vector_stride_B)}; // (NUM_VECTOR_ELEMENTS_B, 1)
+
+    auto copy_A{cute::make_tiled_copy(
+        cute::Copy_Atom<cute::UniversalCopy<VectorTypeA>, TA>{},
+        thread_layout_A,
+        vector_layout_A)}; // Thread layout: (THR_M, THR_K) Value layout:
+                           // (NUM_VECTOR_ELEMENTS_A, 1)
+    auto copy_B{cute::make_tiled_copy(
+        cute::Copy_Atom<cute::UniversalCopy<VectorTypeB>, TB>{},
+        thread_layout_B,
+        vector_layout_B)}; // Thread layout: (THR_N, THR_K) Value layout:
+                           // (NUM_VECTOR_ELEMENTS_B, 1)
+
+    // Swizzle parameters.
+    constexpr int NUM_SHIFT_BITS_A{constexpr_log2(bM)};
+    constexpr int NUM_MASK_BITS_A{constexpr_log2(32)};
+    constexpr int NUM_BASE_BITS_A{constexpr_log2(NUM_VECTOR_ELEMENTS_A)};
+
+    constexpr int NUM_SHIFT_BITS_B{constexpr_log2(bN)};
+    constexpr int NUM_MASK_BITS_B{constexpr_log2(32)};
+    constexpr int NUM_BASE_BITS_B{constexpr_log2(NUM_VECTOR_ELEMENTS_B)};
+
+    auto const swizzle_A{
+        cute::Swizzle<NUM_MASK_BITS_A, NUM_BASE_BITS_A, NUM_SHIFT_BITS_A>{}};
+    auto const swizzle_B{
+        cute::Swizzle<NUM_MASK_BITS_B, NUM_BASE_BITS_B, NUM_SHIFT_BITS_B>{}};
+
+    auto const smem_layout_A_swizzled{
+        cute::composition(swizzle_A, smem_layout_A)};
+    auto const smem_layout_B_swizzled{
+        cute::composition(swizzle_B, smem_layout_B)};
+
+    // Launch the kernel.
+    dim3 const block_dims{
+        static_cast<unsigned int>(cute::size(thread_layout_C))};
+    dim3 const grid_dims{
+        static_cast<unsigned int>(cute::size(cute::ceil_div(M, bM))),
+        static_cast<unsigned int>(cute::size(cute::ceil_div(N, bN)))};
+
+    auto const smem_layout_to_use_A{use_swizzle_A ? smem_layout_A_swizzled
+                                                  : smem_layout_A};
+    auto const smem_layout_to_use_B{use_swizzle_B ? smem_layout_B_swizzled
+                                                  : smem_layout_B};
+
+    general_matrix_multiplication_naive<<<grid_dims, block_dims, 0, stream>>>(
+        gemm_shape, cta_tiler, A, stride_A, smem_layout_to_use_A,
+        thread_layout_A, B, stride_B, smem_layout_to_use_B, thread_layout_B, C,
+        stride_C, smem_layout_C, thread_layout_C, alpha, beta);
+
+    return cudaGetLastError();
+}
+
 // The shape of A is (M, K) and the shape of B is (K, N).
 // Then A is (M, K) column-major and B is (K, N) column-major.
 // Then A is (M, K) column-major and B is (N, K) row-major.
@@ -954,8 +1090,8 @@ static cudaError_t gemm_nt(int m, int n, int k, Alpha alpha, TA const* A,
     CUTE_STATIC_ASSERT_V(cute::size(thread_layout_A) ==
                          cute::size(thread_layout_C));
 
-    using VECTOR_TYPE = cute::uint128_t;
-    constexpr auto NUM_VECTOR_ELEMENTS_A{sizeof(VECTOR_TYPE) / sizeof(TA)};
+    using VECTOR_TYPE_A = cute::uint128_t;
+    constexpr auto NUM_VECTOR_ELEMENTS_A{sizeof(VECTOR_TYPE_A) / sizeof(TA)};
     auto const vector_shape_A{
         cute::make_shape(cute::Int<NUM_VECTOR_ELEMENTS_A>{},
                          cute::Int<1>{})}; // (NUM_VECTOR_ELEMENTS_A, 1)
@@ -964,11 +1100,12 @@ static cudaError_t gemm_nt(int m, int n, int k, Alpha alpha, TA const* A,
     auto const vector_layout_A{cute::make_layout(
         vector_shape_A, vector_stride_A)}; // (NUM_VECTOR_ELEMENTS_A, 1)
     auto copy_A{cute::make_tiled_copy(
-        cute::Copy_Atom<cute::UniversalCopy<VECTOR_TYPE>, TA>{},
+        cute::Copy_Atom<cute::UniversalCopy<VECTOR_TYPE_A>, TA>{},
         thread_layout_A,
         vector_layout_A)}; // Thread layout: (THR_M, THR_K) Value layout:
                            // (NUM_VECTOR_ELEMENTS_A, 1)
-    auto const NUM_VECTOR_ELEMENTS_B{sizeof(VECTOR_TYPE) / sizeof(TB)};
+    using VECTOR_TYPE_B = cute::uint128_t;
+    auto const NUM_VECTOR_ELEMENTS_B{sizeof(VECTOR_TYPE_B) / sizeof(TB)};
     auto const vector_shape_B{
         cute::make_shape(cute::Int<NUM_VECTOR_ELEMENTS_B>{},
                          cute::Int<1>{})}; // (NUM_VECTOR_ELEMENTS_B, 1)
@@ -977,7 +1114,7 @@ static cudaError_t gemm_nt(int m, int n, int k, Alpha alpha, TA const* A,
     auto const vector_layout_B{cute::make_layout(
         vector_shape_B, vector_stride_B)}; // (NUM_VECTOR_ELEMENTS_B, 1)
     auto copy_B{cute::make_tiled_copy(
-        cute::Copy_Atom<cute::UniversalCopy<VECTOR_TYPE>, TB>{},
+        cute::Copy_Atom<cute::UniversalCopy<VECTOR_TYPE_B>, TB>{},
         thread_layout_B,
         vector_layout_B)}; // Thread layout: (THR_N, THR_K) Value layout:
                            // (NUM_VECTOR_ELEMENTS_B, 1)
@@ -1012,8 +1149,136 @@ static cudaError_t gemm_tn(int m, int n, int k, Alpha alpha, TA const* A,
     // C is (M, N) column-major.
     auto const stride_C{cute::make_stride(cute::Int<1>{}, ldC)}; // column-major
 
-    return gemm_base(m, n, k, alpha, A, ldA, B, ldB, beta, C, ldC, stride_A,
-                     stride_B, stride_C, stream);
+    // Because the shared memory layout is (BLK_M, BLK_K) column-major and
+    // the global memory layout is (M, K) row-major, a transpose is needed and
+    // vectorized memory copy is not possible. This transpose will result in
+    // shared memory bank conflicts if not padding or swizzling is used. Another
+    // strategy is to make the shared memory layout (BLK_M, BLK_K) row-major and
+    // then we could perform vectorized memory copy. However, even with
+    // swizzling or padding, there are can still be shared memory bank
+    // conflicts. See https://leimao.github.io/blog/CuTe-Swizzle/ for more
+    // information. So it is matter of experimentation to find the best strategy
+    // for a specific problem. For this example, we will use the first strategy
+    // without thoroughly investigating which strategy is better.
+    using VECTOR_TYPE_A = TA;
+    using VECTOR_TYPE_B = TB;
+
+    // Define GEMM shape.
+    auto const M{m};
+    auto const N{n};
+    auto const K{k};
+    auto const gemm_shape{cute::make_shape(M, N, K)}; // (M, N, K)
+
+    // Define CTA size.
+    auto const bM{cute::Int<128>{}};
+    auto const bN{cute::Int<128>{}};
+    auto const bK{cute::Int<8>{}};
+    auto const cta_tiler{cute::make_shape(bM, bN, bK)}; // (BLK_M, BLK_N, BLK_K)
+
+    // Define smem layouts.
+    // smem_layout_A is (BLK_M, BLK_K) column-major.
+    // smem_layout_B is (BLK_N, BLK_K) column-major.
+    // smem_layout_C is (BLK_M, BLK_N) column-major.
+    auto const smem_shape_A{cute::make_shape(bM, bK)}; // (BLK_M, BLK_K)
+    auto const smem_stride_A{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(smem_shape_A))}; // column-major
+    auto const smem_layout_A{
+        cute::make_layout(smem_shape_A, smem_stride_A)}; // (BLK_M, BLK_K)
+    auto const smem_shape_B{cute::make_shape(bN, bK)};   // (BLK_N, BLK_K)
+    auto const smem_stride_B{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(smem_shape_B))}; // column-major
+    auto const smem_layout_B{
+        cute::make_layout(smem_shape_B, smem_stride_B)}; // (BLK_N, BLK_K)
+    auto const smem_shape_C{cute::make_shape(bM, bN)};   // (BLK_M, BLK_N)
+    auto const smem_stride_C{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(smem_shape_C))}; // column-major
+    auto const smem_layout_C{
+        cute::make_layout(smem_shape_C, smem_stride_C)}; // (BLK_M, BLK_N)
+
+    // Define thread layouts.
+    auto const thread_shape_A{
+        cute::make_shape(cute::Int<32>{}, cute::Int<8>{})}; // (THR_M, THR_K)
+    auto const thread_shape_B{
+        cute::make_shape(cute::Int<32>{}, cute::Int<8>{})}; // (THR_N, THR_K)
+    auto const thread_shape_C{
+        cute::make_shape(cute::Int<16>{}, cute::Int<16>{})}; // (THR_M, THR_N)
+    auto const thread_stride_A{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(thread_shape_A))}; // column-major
+    auto const thread_stride_B{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(thread_shape_B))}; // column-major
+    auto const thread_stride_C{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(thread_shape_C))}; // column-major
+    auto const thread_layout_A{
+        cute::make_layout(thread_shape_A, thread_stride_A)}; // (THR_M, THR_K)
+    auto const thread_layout_B{
+        cute::make_layout(thread_shape_B, thread_stride_B)}; // (THR_N, THR_K)
+    auto const thread_layout_C{
+        cute::make_layout(thread_shape_C, thread_stride_C)}; // (THR_M, THR_N)
+    CUTE_STATIC_ASSERT_V(cute::size(thread_layout_A) ==
+                         cute::size(thread_layout_B));
+    CUTE_STATIC_ASSERT_V(cute::size(thread_layout_A) ==
+                         cute::size(thread_layout_C));
+
+    constexpr auto NUM_VECTOR_ELEMENTS_A{sizeof(VECTOR_TYPE_A) / sizeof(TA)};
+    auto const vector_shape_A{
+        cute::make_shape(cute::Int<NUM_VECTOR_ELEMENTS_A>{},
+                         cute::Int<1>{})}; // (NUM_VECTOR_ELEMENTS_A, 1)
+    auto const vector_stride_A{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(vector_shape_A))}; // column-major
+    auto const vector_layout_A{cute::make_layout(
+        vector_shape_A, vector_stride_A)}; // (NUM_VECTOR_ELEMENTS_A, 1)
+    auto const NUM_VECTOR_ELEMENTS_B{sizeof(VECTOR_TYPE_B) / sizeof(TB)};
+    auto const vector_shape_B{
+        cute::make_shape(cute::Int<NUM_VECTOR_ELEMENTS_B>{},
+                         cute::Int<1>{})}; // (NUM_VECTOR_ELEMENTS_B, 1)
+    auto const vector_stride_B{cute::make_stride(
+        cute::Int<1>{}, cute::size<0>(vector_shape_B))}; // column-major
+    auto const vector_layout_B{cute::make_layout(
+        vector_shape_B, vector_stride_B)}; // (NUM_VECTOR_ELEMENTS_B, 1)
+
+    auto copy_A{cute::make_tiled_copy(
+        cute::Copy_Atom<cute::UniversalCopy<VECTOR_TYPE_A>, TA>{},
+        thread_layout_A,
+        vector_layout_A)}; // Thread layout: (THR_M, THR_K) Value layout:
+                           // (NUM_VECTOR_ELEMENTS_A, 1)
+    auto copy_B{cute::make_tiled_copy(
+        cute::Copy_Atom<cute::UniversalCopy<VECTOR_TYPE_B>, TB>{},
+        thread_layout_B,
+        vector_layout_B)}; // Thread layout: (THR_N, THR_K) Value layout:
+                           // (NUM_VECTOR_ELEMENTS_B, 1)
+
+    // Swizzle parameters.
+    constexpr int NUM_SHIFT_BITS_A{constexpr_log2(bM)};
+    constexpr int NUM_MASK_BITS_A{constexpr_log2(32)};
+    constexpr int NUM_BASE_BITS_A{constexpr_log2(NUM_VECTOR_ELEMENTS_A)};
+
+    constexpr int NUM_SHIFT_BITS_B{constexpr_log2(bN)};
+    constexpr int NUM_MASK_BITS_B{constexpr_log2(32)};
+    constexpr int NUM_BASE_BITS_B{constexpr_log2(NUM_VECTOR_ELEMENTS_B)};
+
+    auto const swizzle_A{
+        cute::Swizzle<NUM_MASK_BITS_A, NUM_BASE_BITS_A, NUM_SHIFT_BITS_A>{}};
+    auto const swizzle_B{
+        cute::Swizzle<NUM_MASK_BITS_B, NUM_BASE_BITS_B, NUM_SHIFT_BITS_B>{}};
+
+    auto const smem_layout_A_swizzled{
+        cute::composition(swizzle_A, smem_layout_A)};
+    auto const smem_layout_B_swizzled{
+        cute::composition(swizzle_B, smem_layout_B)};
+
+    // Launch the kernel.
+    dim3 const block_dims{
+        static_cast<unsigned int>(cute::size(thread_layout_C))};
+    dim3 const grid_dims{
+        static_cast<unsigned int>(cute::size(cute::ceil_div(M, bM))),
+        static_cast<unsigned int>(cute::size(cute::ceil_div(N, bN)))};
+
+    general_matrix_multiplication_naive<<<grid_dims, block_dims, 0, stream>>>(
+        gemm_shape, cta_tiler, A, stride_A, smem_layout_A_swizzled,
+        thread_layout_A, B, stride_B, smem_layout_B_swizzled, thread_layout_B,
+        C, stride_C, smem_layout_C, thread_layout_C, alpha, beta);
+
+    return cudaGetLastError();
 }
 
 // The shape of transposed A is (M, K) and the shape of transposed B is (K, N).
