4957317fcf175653134d9da9d378500b0422cd97
leimao <dukeleimao@gmail.com>
2024-12-26 20:45:57 -0800

    Add New Files

--- examples/cute_general_matrix_multiplication/cute_general_matrix_multiplication_naive_tiled_copy_tiled_mma_sm70_pipeline.cu
+++ examples/cute_general_matrix_multiplication/cute_general_matrix_multiplication_naive_tiled_copy_tiled_mma_sm70_pipeline.cu
@@ -131,11 +131,21 @@ static __global__ void general_matrix_multiplication_tiled_copy_tiled_mma(
         global_block_tensor_A)}; // (CPY, CPY_M, CPY_K, k)
     auto thread_layout_A_smem_tensor_A{
         thread_copy_A.partition_D(smem_tensor_A)}; // (CPY, CPY_M, CPY_K)
+    // Loading data from global memory to register while shared memory is being
+    // loaded. Instead of using double shared memory buffer, we used more
+    // registers for pipelining.
+    auto thread_layout_A_register_tensor_A{cute::make_fragment_like(
+        thread_layout_A_smem_tensor_A)}; // (CPY, CPY_M, CPY_K)
     auto thread_copy_B{copy_B.get_slice(threadIdx.x)};
     auto thread_layout_B_global_block_tensor_B{thread_copy_B.partition_S(
         global_block_tensor_B)}; // (CPY, CPY_N, CPY_K, k)
     auto thread_layout_B_smem_tensor_B{
         thread_copy_B.partition_D(smem_tensor_B)}; // (CPY, CPY_N, CPY_K)
+    // Loading data from global memory to register while shared memory is being
+    // loaded. Instead of using double shared memory buffer, we used more
+    // registers for pipelining.
+    auto thread_layout_B_register_tensor_B{cute::make_fragment_like(
+        thread_layout_B_smem_tensor_B)}; // (CPY, CPY_N, CPY_K)
 
     // Partition via MMA.
     auto thread_mma{mma.get_slice(threadIdx.x)};
@@ -149,6 +159,10 @@ static __global__ void general_matrix_multiplication_tiled_copy_tiled_mma(
     // Allocate the accumulators.
     // The layout is automatically compacted to the smallest possible layout to
     // avoid unnecessary memory/register usage.
+    auto thread_layout_C_register_tensor_A{cute::make_fragment_like(
+        thread_layout_C_smem_tensor_A)}; // (MMA, MMA_M, MMA_K)
+    auto thread_layout_C_register_tensor_B{cute::make_fragment_like(
+        thread_layout_C_smem_tensor_B)}; // (MMA, MMA_N, MMA_K)
     auto thread_layout_C_register_tensor_C{cute::make_fragment_like(
         thread_layout_C_global_block_tensor_C)}; // (MMA, MMA_M, MMA_N)
 
@@ -236,42 +250,113 @@ static __global__ void general_matrix_multiplication_tiled_copy_tiled_mma(
                 cute::size<1>(shape_MNK);
     }
 
+    // Perform SM70 pipelining.
+
+    // Prefetch.
+    // Copy from global memory to shared memory for tile_idx_k = 0.
+    cute::clear(thread_layout_A_register_tensor_A);
+    cute::clear(thread_layout_B_register_tensor_B);
+    // Need predicates for bounds checking.
+    cute::copy_if(
+        copy_A, thread_layout_A_predicate_tensor_A,
+        thread_layout_A_global_block_tensor_A(cute::_, cute::_, cute::_, 0),
+        thread_layout_A_register_tensor_A);
+    cute::copy_if(
+        copy_B, thread_layout_B_predicate_tensor_B,
+        thread_layout_B_global_block_tensor_B(cute::_, cute::_, cute::_, 0),
+        thread_layout_B_register_tensor_B);
+    // Prepare the shared memory for the first tile iteration.
+    // Copy from register to shared memory for tile_idx_k = 0.
+    // No need for predicates and clear for shared memory anymore.
+    cute::copy(thread_layout_A_register_tensor_A,
+               thread_layout_A_smem_tensor_A);
+    cute::copy(thread_layout_B_register_tensor_B,
+               thread_layout_B_smem_tensor_B);
+    // Synchronize to ensure the data on shared memory is ready for mma.
+    __syncthreads();
+
     // Perform the gemm computation loop.
     auto const num_tiles_k{cute::size<2>(global_block_tensor_A)}; // k
+    constexpr auto num_mmas_per_tile_k{
+        cute::size<2>(thread_layout_C_register_tensor_A)}; // MMA_K
+    // Prepare the registers for the first mma iteration.
+    cute::copy(thread_layout_C_smem_tensor_A(cute::_, cute::_, 0),
+               thread_layout_C_register_tensor_A(cute::_, cute::_, 0));
+    cute::copy(thread_layout_C_smem_tensor_B(cute::_, cute::_, 0),
+               thread_layout_C_register_tensor_B(cute::_, cute::_, 0));
+
+    // We want to overlap data copy and compute.
+    // The thread performs mma iteration inside the tile iteration.
+    // 1. At the beginning of each tile iteration (the first mma iteration of
+    // each tile iteration), the thread copies data from global memory to
+    // register for the next tile iteration, asynchronously, and this can take a
+    // while.
+    // 2. In each mma iteration, the thread copy data from shared memory to
+    // register for the next mma iteration, and perform mma for the current mma
+    // iteration, asynchronously.
+    // 3. Before the last mma iteration of each tile iteration, hopefully the
+    // data copy from global memory to register in #1 is completed,
+    // synchronization is performed. The thread copies data from the register to
+    // shared memory, asynchronously. Synchronization is performed again to
+    // ensure the data for the mma of the new tile iteration is ready.
 
     for (auto tile_idx_k{0}; tile_idx_k < num_tiles_k; ++tile_idx_k)
     {
-        // Clear the shared memory buffers.
-        // This is necessary when predicates are used for copying data from
-        // global memory to shared memory so that mma will not be affected by
-        // the previous data in the unwanted region.
-        cute::clear(thread_layout_A_smem_tensor_A);
-        cute::clear(thread_layout_B_smem_tensor_B);
-
-        cute::copy_if(copy_A, thread_layout_A_predicate_tensor_A,
-                      thread_layout_A_global_block_tensor_A(
-                          cute::_, cute::_, cute::_, tile_idx_k),
-                      thread_layout_A_smem_tensor_A);
-        cute::copy_if(copy_B, thread_layout_B_predicate_tensor_B,
-                      thread_layout_B_global_block_tensor_B(
-                          cute::_, cute::_, cute::_, tile_idx_k),
-                      thread_layout_B_smem_tensor_B);
-
-        // Synchronize the threads to ensure the data copy is completed.
-        cute::cp_async_fence();
-        cute::cp_async_wait<0>();
-        __syncthreads();
-
-        // Compute gemm on thread_layout_C thread-partitioned smem.
-        // This implicitly uses the UniversalFMA GEMM atom.
-        cute::gemm(mma, thread_layout_C_smem_tensor_A,
-                   thread_layout_C_smem_tensor_B,
-                   thread_layout_C_register_tensor_C); // (BLK_M / THR_M, BLK_N
-                                                       // / THR_N) += (BLK_M /
-                                                       // THR_M, BLK_K) * (BLK_N
-                                                       // / THR_N, BLK_K)
-
-        __syncthreads();
+        CUTE_UNROLL
+        for (auto mma_idx_k{0}; mma_idx_k < num_mmas_per_tile_k; ++mma_idx_k)
+        {
+            // Before the last mma iteration of each tile iteration, copy data
+            // from register to shared memory for the next tile iteration.
+            if (mma_idx_k == num_mmas_per_tile_k - 1)
+            {
+                // Ensure the data copy from global memory to register is
+                // completed.
+                __syncthreads();
+                // Copy data from register to shared memory for the next tile
+                // iteration.
+                cute::copy(thread_layout_A_register_tensor_A,
+                           thread_layout_A_smem_tensor_A);
+                cute::copy(thread_layout_B_register_tensor_B,
+                           thread_layout_B_smem_tensor_B);
+                // Ensure the data for the mma of the new tile iteration is
+                // ready.
+                __syncthreads();
+            }
+            // Copy data from shared memory to register for the next mma
+            // iteration.
+            auto const mma_idx_k_next{(mma_idx_k + 1) % num_mmas_per_tile_k};
+            cute::copy(
+                thread_layout_C_smem_tensor_A(cute::_, cute::_, mma_idx_k_next),
+                thread_layout_C_register_tensor_A(cute::_, cute::_,
+                                                  mma_idx_k_next));
+            cute::copy(
+                thread_layout_C_smem_tensor_B(cute::_, cute::_, mma_idx_k_next),
+                thread_layout_C_register_tensor_B(cute::_, cute::_,
+                                                  mma_idx_k_next));
+            // Before the first mma iteration of each tile iteration, copy data
+            // from global memory to register for the next tile iteration.
+            if (mma_idx_k == 0)
+            {
+                auto const tile_idx_k_next{(tile_idx_k + 1) % num_tiles_k};
+                cute::clear(thread_layout_A_register_tensor_A);
+                cute::clear(thread_layout_B_register_tensor_B);
+                // Need predicates for bounds checking.
+                cute::copy_if(copy_A, thread_layout_A_predicate_tensor_A,
+                              thread_layout_A_global_block_tensor_A(
+                                  cute::_, cute::_, cute::_, tile_idx_k_next),
+                              thread_layout_A_register_tensor_A);
+                cute::copy_if(copy_B, thread_layout_B_predicate_tensor_B,
+                              thread_layout_B_global_block_tensor_B(
+                                  cute::_, cute::_, cute::_, tile_idx_k_next),
+                              thread_layout_B_register_tensor_B);
+            }
+            // Perform mma for the current mma iteration.
+            cute::gemm(
+                mma,
+                thread_layout_C_register_tensor_A(cute::_, cute::_, mma_idx_k),
+                thread_layout_C_register_tensor_B(cute::_, cute::_, mma_idx_k),
+                thread_layout_C_register_tensor_C);
+        }
     }
 
     // Scale and accumulate the result from the register tensor to the global
